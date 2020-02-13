8f65ba3f91dd9b21933303cfaf8b6bac95b30fbe
Chris Masters <chrismasters@gmail.com>
2020-02-13 14:33:09 +0100

    Updated script

--- countries-to-csv.rb
+++ countries-to-csv.rb
@@ -11,7 +11,7 @@ SECRET_KEY = ENV['SECRET_KEY']
 
 BASE_API_URL = "https://api2.radio.orange.com"
 
-CONTINENT_CODE = ARGV[0].to_s
+# CONTINENT_CODE = ARGV[0].to_s
 
 def getJsonFromFile filepath
   file = File.open filepath
@@ -19,17 +19,17 @@ def getJsonFromFile filepath
   return json
 end
 
-def generateCSV(json, lang)
-  en_json = getJsonFromUrl("/v2/localisations/#{CONTINENT_CODE}/countries", "en-EN")
+def generateCSV(json, lang, continent)
+  en_json = getJsonFromUrl("/v2/localisations/#{continent}/countries", "en-EN")
   csv_string = CSV.generate do |csv|
-    header = ["Code", "Name", "Radio count", "Review"]
+    header = ["Code", "Name"]
     header.insert(2, "Name (EN)") unless lang == "en-EN"
     csv << header
     json["result"].each do |item|
-      country_json = getJsonFromUrl("/v2/tops/radios/#{item["code"]}", lang)
-      country_count = country_json["paginate"]["count"]
+      # country_json = getJsonFromUrl("/v2/tops/radios/#{item["code"]}", lang)
+      # country_count = country_json["paginate"]["count"]
       en_name = en_json["result"].find { |key| key["code"] == item["code"] }["name"]
-      row = [item["code"],item["name"],country_count]
+      row = [item["code"],item["name"]]
       row.insert(2, en_name) unless lang == "en-EN"
       csv << row
     end
@@ -61,12 +61,17 @@ def getJsonFromUrl(request_url, lang)
   return JSON.parse(resp.body)
 end
 
-languages = ["en-EN", "ar-AR", "fr-FR", "es-ES", "nl-NL", "pt-PT"]
+continents = ["AF", "AS", "EU", "NA", "SA", "OC", "AN"]
 
-languages.each do |language|
-  json = getJsonFromUrl("/v2/localisations/#{CONTINENT_CODE}/countries", language)
-  puts "### #{language}"
-  puts ""
-  puts generateCSV(json, language)
-  puts ""
+languages = ["en-EN"]
+
+continents.each do |continent|
+  puts "### #{continent}"
+  languages.each do |language|
+    json = getJsonFromUrl("/v2/localisations/#{continent}/countries", language)
+    puts "### #{language}"
+    puts ""
+    puts generateCSV(json, language, continent)
+    puts ""
+  end
 end
